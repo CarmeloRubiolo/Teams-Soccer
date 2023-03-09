d769f747072d96abf91f66b25f847b5d67d6165b
Carmelo Rubiolo <melo_rubiolo@hotmail.com>
2023-03-09 20:29:14 -0300

    actualizacion de equipos

--- src/componentes/TeamsDetailContainer/TeamsDetailContainer.jsx
+++ src/componentes/TeamsDetailContainer/TeamsDetailContainer.jsx
@@ -55,7 +55,23 @@ const TeamsDetailContainer = () => {
                                <span className="subtitle">Presidente</span>  <br/> <p className="detail-data text-center">{teams.presidente}</p>
                             </div>
                             <div className="col-12 text-center mb-5">
-                               <span className="subtitle">Equipacion</span>  <br/> <img className="img-equipacion" src={teams.equipacion} alt={teams.name} />
+                               <span className="subtitle">Equipacion</span>  <br/> <div type="button" data-bs-toggle="modal" data-bs-target="#exampleModal2"><img className="img-equipacion" src={teams.equipacion} alt={teams.name} /></div>
+                               <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1>Equipación</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <img className="imgEquipacion-modal" src={teams.equipacion} alt={teams.name} />
+                                    </div>
+                                    {/* <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div> */}
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                             <div className="col-12 text-center mb-5">
                                <span className="subtitle">Estadio</span>  <br/> <div type="button" data-bs-toggle="modal" data-bs-target="#exampleModal"><img className=" img-estadio mt-2" src={teams.imgEstadio} alt={teams.name}></img></div>
